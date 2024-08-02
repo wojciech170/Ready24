f76b580d33d0c5dda6e377f6a62b11fb6e6ba579
wojciech170 <wojciech170@gmail.com>
2024-08-02 10:38:21 +0000

    adjust migration name  and dependecies for consistence

--- shop/migrations/0005_alter_category_slug_alter_product_slug.py
+++ shop/migrations/0005_alter_category_slug_alter_product_slug.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shop', 'populate_slugs'),
+        ('shop', '0004_populate_slugs'),
     ]
 
     operations = [
